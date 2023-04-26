131d968ce8db58f47d167738cbd32f2927b5bc0a
Shweta Gupta <shwetarg.it@gmail.com>
2023-04-26 17:50:52 -0400

    created deal card function to choose cards

--- Blackjack_game/blackjack_main.py
+++ Blackjack_game/blackjack_main.py
@@ -9,3 +9,13 @@
 # The cards in the list have equal probability of being drawn.
 # Cards are not removed from the deck as they are drawn.
 # The computer is the dealer.
+# Create a deal_card() function that uses the List below to *return* a random card.
+# 11 is the Ace.
+import random
+
+
+def deal_card():
+    # cards = [11, 10]
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    random_card = random.choice(cards)
+    return random_card
