8d26f5fceea196bc9527c7223ce26f75990b79eb
Shweta Gupta <shwetarg.it@gmail.com>
2023-04-26 17:51:43 -0400

    choose cards

--- Blackjack_game/blackjack_main.py
+++ Blackjack_game/blackjack_main.py
@@ -19,3 +19,15 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     random_card = random.choice(cards)
     return random_card
+
+
+game_ends = False
+user_cards = []
+computer_cards = []
+user_cards.append(deal_card())
+user_cards.append(deal_card())
+
+print(user_cards)
+computer_cards.append(deal_card())
+computer_cards.append(deal_card())
+print(computer_cards)
