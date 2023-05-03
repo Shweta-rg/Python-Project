5a3416424aa7aa03ca679f231ae85b5d4c2411bb
Shweta Gupta <shwetarg.it@gmail.com>
2023-05-03 18:47:08 -0400

    added 3 more decorators

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,28 @@
 from flask import Flask
 
 app = Flask(__name__)
+# made decorator make_bold make_empahsis make_underlined
+
+
+def make_bold(function):
+    def wrapper():
+        bold_text = function()
+        return "<b>"+function() + "</b>"
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        bold_text = function()
+        return "<u>"+function() + "</u>"
+    return wrapper
+
+
+def make_empahsis(function):
+    def wrapper():
+        bold_text = function()
+        return "<em>"+function() + "</em>"
+    return wrapper
 
 
 @app.route('/')
@@ -9,8 +31,11 @@ def hello_world():
 
 
 @app.route("/bye")
+@make_bold
+@make_underlined
+@make_empahsis
 def say_bye():
-    return "<u><b>Bye<b><u>"
+    return "Bye"
 
 # after putting username in url whateven two input comes up in function
 
