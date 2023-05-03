d5b03777733cc1230d0d3eda99540afd8dd2b4f1
Shweta Gupta <shwetarg.it@gmail.com>
2023-05-03 18:34:26 -0400

    added two differnet input names and also debugger

--- Flask/main.py
+++ Flask/main.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route("/bye")
 def say_bye():
-    return "Bye"
+    return "<u><b>Bye<b><u>"
+
+# after putting username in url whateven two input comes up in function
+
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old!."
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
